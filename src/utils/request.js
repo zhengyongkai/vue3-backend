7b7b366bdbff38ab52a02d8e5204d402dc9236f6
zhengyongkai <825947557@qq.com>
2022-07-14 00:00:53 +0800

    🦄 refactor(request.js): 修改前端与后端的api响应
    修改 request.js 中 response 对 code 修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,36 +1,35 @@
-import axios from "axios";
-import { ElNotification } from "element-plus";
-import { useRouter } from "vue-router";
-import qs from 'qs'
-import {getParamsNotNull} from '@/utils/utils'
+import axios from 'axios';
+import { ElNotification } from 'element-plus';
+import { useRouter } from 'vue-router';
+import qs from 'qs';
+import { getParamsNotNull } from '@/utils/utils';
 const service = axios.create({
   // process.env.NODE_ENV === 'development'
   timeout: 5000,
 });
-const env = 'development'
-const api = env === 'development' ? '/Api' : ''
+const env = 'development';
+const api = env === 'development' ? '/Api' : '';
 service.interceptors.request.use(
   (config) => {
     const token = window.localStorage.getItem('storage_tokenList');
-    config.url = api + config.url 
-    if (config.method === "post" && !config.file ) {
+    config.url = api + config.url;
+    if (config.method === 'post' && !config.file) {
       config.headers = {
-        "content-type": "application/json",
+        'content-type': 'application/json',
       };
     }
-    console.log(config)
-    if(config.file){
+    if (config.file) {
       config.headers = {
-        "content-type": "multipart/form-data",
+        'content-type': 'multipart/form-data',
       };
     }
-    if(config.method === 'get'){
-      config.params = getParamsNotNull(config.params)
+    if (config.method === 'get') {
+      config.params = getParamsNotNull(config.params);
     }
-    if(config.url.indexOf('login') == -1){
-        if (token) {
-            config.headers['authorization'] = `${token}`;
-        }
+    if (config.url.indexOf('login') == -1) {
+      if (token) {
+        config.headers['authorization'] = `${token}`;
+      }
     }
     return config;
   },
@@ -40,27 +39,24 @@ service.interceptors.request.use(
 );
 
 service.interceptors.response.use(
-  (response) => {
-    if (response.status === 200) {
-      if (response.data.code === 200) {
-        return response.data;
-      } else {
-        if (response.data.code === 401) {
-             location.href='/#/login'
-        }
-        ElNotification({
-          message: response.data.msg,
-          type: "error",
-        });
-        return Promise.reject();
+  ({ status, data }) => {
+    if (status === 200) {
+      if (data.code === 200) {
+        return data;
       }
-     
-    } else {
-      Promise.reject();
+      if (data.code === 401) {
+        location.href = '/#/login';
+      }
+      if (data.code === 500) {
+        return ElNotification({
+          message: data.msg,
+          type: 'error',
+        });
+      }
+      return Promise.reject();
     }
   },
   (error) => {
-    console.log(error);
     return Promise.reject();
   }
 );
